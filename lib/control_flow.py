0a3f7d5c73bebd4c8949d11028787ef09a84947d
victor <victor.njutha@student.moringaschool.com>
2023-12-09 22:58:10 +0300

    completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,55 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    # Check if the username is "admin" or "ADMIN" and the password is "12345"
+    if (username.lower() == "admin" or username.upper() == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
+# Example usage:
+username_input = input("Enter username: ")
+password_input = input("Enter password: ")
+
+result = admin_login(username_input, password_input)
+print(result)
+
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature < 65:
+        return "It's a little chilly out there!"
+    elif 65 <= temperature <= 85:
+        return "It's perfect out there!"
+    else:
+        return "It's too dang hot out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
-def calculator(operation, num1, num2):
-    # your code here
-    pass
+
+def calculator(operator, num1, num2):
+    try:
+        if operator == "+":
+            return num1 + num2
+        elif operator == "-":
+            return num1 - num2
+        elif operator == "*":
+            return num1 * num2
+        elif operator == "/":
+            return num1 / num2
+        else:
+            print("Invalid operation!")
+            return None
+    except ZeroDivisionError:
+        print("Error: Division by zero!")
+        return None
